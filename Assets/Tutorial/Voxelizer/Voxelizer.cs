082ec95be723265ff485cf58620539694db99a6a
skmang <muwei5231@gmail.com>
2019-09-22 21:50:52 +0800

    Add some aabb-triangle intersect algorithm's comment.

--- Assets/Tutorial/Voxelizer/Voxelizer.cs
+++ Assets/Tutorial/Voxelizer/Voxelizer.cs
@@ -96,17 +96,12 @@ public class Voxelizer : MonoBehaviour
                 Vector3 v1 = filter.transform.TransformPoint(vertices[tris[i]]);
                 Vector3 v2 = filter.transform.TransformPoint(vertices[tris[i+1]]);
                 Vector3 v3 = filter.transform.TransformPoint(vertices[tris[i+2]]);
-                Vector3 u = v2 - v3;
-                Vector3 v = v3 - v1;
-                Vector3 normal = Vector3.Cross(u,v).normalized;
-
                 AABBBoundBox box = new AABBBoundBox(Vector3.positiveInfinity,Vector3.negativeInfinity);
                 box.Extend(v1);
                 box.Extend(v2);
                 box.Extend(v3);
                 _triangleList.Add(new TriangleInfo
                 {
-                    normal = normal,
                     p1 = v1,
                     p2 = v2,
                     p3 = v3,
@@ -154,6 +149,7 @@ public class Voxelizer : MonoBehaviour
         Vector3 u2 = new Vector3(0,0,1.0f);
         Vector3[] axiArray = new Vector3[]
         {
+            // 叉积轴
             Vector3.Cross(u0, f0),
             Vector3.Cross(u0, f1),
             Vector3.Cross(u0, f2),
@@ -163,9 +159,11 @@ public class Voxelizer : MonoBehaviour
             Vector3.Cross(u2, f0),
             Vector3.Cross(u2, f1),
             Vector3.Cross(u2, f2),
+            // AABB面法线
             u0,
             u1,
             u2,
+            // 三角形面法线
             Vector3.Cross(f0,f1)
         };
         for (int i = 0; i < axiArray.Length; i++)
@@ -192,7 +190,8 @@ public class Voxelizer : MonoBehaviour
                   aabbExtent.y * Mathf.Abs(Vector3.Dot(u1, axi)) +
                   aabbExtent.z * Mathf.Abs(Vector3.Dot(u2, axi));
 
-        // 是分离轴
+        // aabb投影区间[-r,r] 三角形投影区间[min(p0,p1,p2),max(p0,p1,p2)],如果两个区间没有交集则是分离轴
+        // 这里的比较是一种高效方式 代替 if(min > r || max < -r)
         if (Mathf.Max(
             -Mathf.Max(Mathf.Max(p0,p1),p2),
             Mathf.Min(Mathf.Min(p0,p1),p2)
